83be6b6ca03d230c8b215f4d8f85701c72337de7
TrongTan19062004 <136068548+TrongTan19062004@users.noreply.github.com>
2025-05-02 19:03:52 +0700

    Add files via upload

---
+++ LibrarySystemManagement-master/TransferObject/Borrow.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace TransferObject
+{
+    public class Borrow
+    {
+        public int BorrowID { get; set; }
+        public int ReaderId { get; set; }
+        public int DocumentId { get; set; }
+        public DateTime BorrowDate { get; set; }
+        public DateTime ReturnDate { get; set; }
+        public bool IsReturned { get; set; } = false;
+    }
+}

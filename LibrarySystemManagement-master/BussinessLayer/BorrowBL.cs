f8afeebf7293984860f544d2c3f104c9545242e0
TrongTan19062004 <136068548+TrongTan19062004@users.noreply.github.com>
2025-05-02 19:01:02 +0700

    upload DocumentBL,AccountBL,BorrowBL

---
+++ LibrarySystemManagement-master/BussinessLayer/BorrowBL.cs
@@ -0,0 +1,38 @@
+﻿using DataLayer;
+using TransferObject;
+using System.Collections.Generic;
+
+namespace BusinessLayer
+{
+    public class BorrowBL
+    {
+        private BorrowDL borrowDL = new BorrowDL();
+        private ReaderDL readerDL = new ReaderDL();
+        private DocumentDL documentDL = new DocumentDL();
+
+        public bool BorrowDocument(Borrow borrow)
+        {
+            return borrowDL.InsertBorrow(borrow);
+        }
+
+        public bool MarkAsReturned(int borrowId)
+        {
+            return borrowDL.MarkAsReturned(borrowId);
+        }
+
+        public List<Borrow> GetUnreturnedBorrows()
+        {
+            return borrowDL.GetUnreturnedBorrows();
+        }
+
+        public Reader GetReaderById(int readerId)
+        {
+            return readerDL.GetReaderById(readerId);
+        }
+
+        public Document GetDocumentById(int docId)
+        {
+            return documentDL.GetDocumentById(docId);
+        }
+    }
+}
--- LibrarySystemManagement-master/BussinessLayer/DocumentBL.cs
+++ LibrarySystemManagement-master/BussinessLayer/DocumentBL.cs
@@ -1,3 +1,41 @@
+//<<<<<<< HEAD
+//﻿using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Runtime.Remoting.Messaging;
+//using System.Text;
+//using System.Threading.Tasks;
+//using DataLayer;
+//using TransferObject;
+//namespace BussinessLayer
+//{
+//    public class DocumentBL
+//    {
+//        private DocumentDL DocumentDL = new DocumentDL();
+//        public List<Document> GetDocuments()
+//        {
+//            return DocumentDL.GetDocuments();
+//        }
+//        public bool addDocument(Document document, string authorName, string genreName, string documentType)
+//        {
+
+//            return DocumentDL.addDocument(document, authorName, genreName, documentType);
+//        }
+//        public bool updateDocument(Document document, string authorName, string genreName, string documentType)
+//        {
+//            return DocumentDL.UpdateDocument(document, authorName, genreName, documentType);
+//        }
+//        public Document searchDocument(int id)
+//        {
+//            return DocumentDL.searchDocument(id);
+//        }
+//        public bool deleteDocument(int id)
+//        {
+//            return DocumentDL.deleteDocument(id) > 0;
+//        }
+//    }
+//}
+//=======
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -34,3 +72,4 @@ namespace BussinessLayer
         }
     }
 }
+//>>>>>>> c621290df399d2618a653368b7b720e87a1669a5

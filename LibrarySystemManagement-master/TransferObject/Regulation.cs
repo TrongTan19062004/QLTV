4993186c048ebad0cfbdf5e30fd495ad4668e646
TrongTan19062004 <136068548+TrongTan19062004@users.noreply.github.com>
2025-05-03 20:56:14 +0700

    update TransferObject

---
+++ LibrarySystemManagement-master/TransferObject/Regulation.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TransferObject
+{
+    public class Regulation
+    {
+        // Fields
+        private int regulationID;
+        private string description;
+        private int MaxBorrowDays;
+        private int finePerDay;
+        private int MaxBooksAllowed;
+
+        // Default constructor
+        public Regulation()
+        {
+        }
+
+        // Constructor with parameters
+        public Regulation(int re_Id, string description, int maxBookDays, int finePerDays, int maxBookAllow)
+        {
+            this.regulationID = re_Id;
+            this.Description = description;
+            this.MaxBookDays = maxBookDays;
+            this.finePerDay = finePerDays;
+            this.MaxBooksAllowed = maxBookAllow;
+        }
+
+        // Properties (Getters and Setters)
+        public int RegulationID
+        {
+            get { return regulationID ; }
+            set { regulationID = value; }
+        }
+
+        public string Description
+        {
+            get { return description; }
+            set { description = value; }
+        }
+
+        public int MaxBookDays
+        {
+            get { return MaxBorrowDays; }
+            set { MaxBorrowDays = value; }
+        }
+
+        public int FinePerDay
+        {
+            get { return finePerDay; }
+            set { finePerDay = value; }
+        }
+
+        public int MaxBooksAllow
+        {
+            get { return MaxBooksAllowed; }
+            set { MaxBooksAllowed = value; }
+        }
+    }
+
+}
---
+++ LibrarySystemManagement-master/TransferObject/Reservation.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TransferObject
+{
+    public class Reservation
+    {
+        public int ReservationId { get; set; }
+        public int UserId { get; set; }
+        public int DocumentId { get; set; }
+        public DateTime ReservationDate { get; set; }
+        public string ReservationStatus { get; set; }
+
+        public Reservation() { }
+
+        public Reservation(int reservationId, int userId, int documentId, DateTime reservationDate, string reservationStatus)
+        {
+            ReservationId = reservationId;
+            UserId = userId;
+            DocumentId = documentId;
+            ReservationDate = reservationDate;
+            ReservationStatus = reservationStatus;
+        }
+    }
+
+}

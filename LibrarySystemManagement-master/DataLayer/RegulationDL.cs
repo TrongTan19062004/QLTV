142c2f723c5772e7c5b4331da0cae6149fd5f207
TrongTan19062004 <136068548+TrongTan19062004@users.noreply.github.com>
2025-05-03 20:54:14 +0700

    updateDL

---
+++ LibrarySystemManagement-master/DataLayer/RegulationDL.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using DataLayer; // namespace chứa DataProvier
+using TransferObject;       // namespace chứa class Regulation
+
+public class RegulationDL
+{
+    private DataProvier dataProvider = new DataProvier();
+
+    public List<Regulation> GetAllRegulations()
+    {
+        List<Regulation> regulations = new List<Regulation>();
+        string sql = "SELECT * FROM Regulation";
+
+        DataTable dt = dataProvider.MyExcuteReader(sql, CommandType.Text);
+
+        foreach (DataRow row in dt.Rows)
+        {
+            Regulation reg = new Regulation
+            {
+                RegulationID = Convert.ToInt32(row["RegulationID"]),
+                Description = row["Description"].ToString(),
+                MaxBookDays = Convert.ToInt32(row["MaxBorrowDays"]),
+                FinePerDay = Convert.ToInt32(row["FinePerDay"]),
+                MaxBooksAllow = Convert.ToInt32(row["MaxBooksAllowed"])
+            };
+
+            regulations.Add(reg);
+        }
+
+        return regulations;
+    }
+    // Cập nhật Description
+    // Cập nhật Description
+    public bool UpdateDescription(int regulationId, string description)
+    {
+        string sql = "UPDATE Regulation SET Description = @Description WHERE RegulationID = @RegulationID";
+        SqlParameter[] parameters = new SqlParameter[]
+        {
+        new SqlParameter("@Description", description),
+        new SqlParameter("@RegulationID", regulationId)
+        };
+        return dataProvider.MyExcuteNonQuery(sql, CommandType.Text, parameters) > 0;
+    }
+
+    // Cập nhật MaxBorrowDays
+    public bool UpdateMaxBookDays(int regulationId, int maxBorrowDays)
+    {
+        string sql = "UPDATE Regulation SET MaxBorrowDays = @MaxBorrowDays WHERE RegulationID = @RegulationID";
+        SqlParameter[] parameters = new SqlParameter[]
+        {
+        new SqlParameter("@MaxBorrowDays", maxBorrowDays),
+        new SqlParameter("@RegulationID", regulationId)
+        };
+        return dataProvider.MyExcuteNonQuery(sql, CommandType.Text, parameters) > 0;
+    }
+
+    // Cập nhật FinePerDay
+    public bool UpdateFinePerDays(int regulationId, int finePerDay)
+    {
+        string sql = "UPDATE Regulation SET FinePerDay = @FinePerDay WHERE RegulationID = @RegulationID";
+        SqlParameter[] parameters = new SqlParameter[]
+        {
+        new SqlParameter("@FinePerDay", finePerDay),
+        new SqlParameter("@RegulationID", regulationId)
+        };
+        return dataProvider.MyExcuteNonQuery(sql, CommandType.Text, parameters) > 0;
+    }
+
+    // Cập nhật MaxBooksAllowed
+    public bool UpdateMaxBookAllow(int regulationId, int maxBooksAllowed)
+    {
+        string sql = "UPDATE Regulation SET MaxBooksAllowed = @MaxBooksAllowed WHERE RegulationID = @RegulationID";
+        SqlParameter[] parameters = new SqlParameter[]
+        {
+        new SqlParameter("@MaxBooksAllowed", maxBooksAllowed),
+        new SqlParameter("@RegulationID", regulationId)
+        };
+        return dataProvider.MyExcuteNonQuery(sql, CommandType.Text, parameters) > 0;
+    }
+
+
+}
---
+++ LibrarySystemManagement-master/DataLayer/ReservationDL.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TransferObject;
+
+namespace DataLayer
+{
+    public class ReservationDL
+    {
+        private DataProvier db;
+
+        public ReservationDL()
+        {
+            db = new DataProvier();
+        }
+
+        public List<Reservation> GetAllReservations()
+        {
+            string sql = "SELECT * FROM Reservation";
+            DataTable dt = db.MyExcuteReader(sql, CommandType.Text);
+            List<Reservation> list = new List<Reservation>();
+            foreach (DataRow row in dt.Rows)
+            {
+                Reservation res = new Reservation(
+                    Convert.ToInt32(row["reservation_id"]),
+                    Convert.ToInt32(row["user_id"]),
+                    Convert.ToInt32(row["document_id"]),
+                    Convert.ToDateTime(row["reservation_date"]),
+                    row["reservation_status"].ToString()
+                );
+                list.Add(res);
+            }
+            return list;
+        }
+
+        public bool AddReservation(Reservation res)
+        {
+            string sql = "INSERT INTO Reservation (user_id, document_id, reservation_date, reservation_status) " +
+                         "VALUES (@user_id, @document_id, @reservation_date, @reservation_status)";
+            SqlParameter[] parameters = {
+            new SqlParameter("@user_id", res.UserId),
+            new SqlParameter("@document_id", res.DocumentId),
+            new SqlParameter("@reservation_date", DateTime.Now),
+            new SqlParameter("@reservation_status", res.ReservationStatus)
+        };
+            return db.MyExcuteNonQuery(sql, CommandType.Text, parameters) > 0;
+        }
+
+        public bool UpdateReservationStatus(int reservationId, string status)
+        {
+            string sql = "UPDATE Reservation SET reservation_status = @status WHERE reservation_id = @id";
+            SqlParameter[] parameters = {
+            new SqlParameter("@status", status),
+            new SqlParameter("@id", reservationId)
+        };
+            return db.MyExcuteNonQuery(sql, CommandType.Text, parameters) > 0;
+        }
+
+        public bool DeleteReservation(int reservationId)
+        {
+            string sql = "DELETE FROM Reservation WHERE reservation_id = @id";
+            SqlParameter[] parameters = {
+            new SqlParameter("@id", reservationId)
+        };
+            return db.MyExcuteNonQuery(sql, CommandType.Text, parameters) > 0;
+        }
+    }
+}

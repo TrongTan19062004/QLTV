b9c3694d011d72fb35d7b0bab56284a34933ff71
TrongTan19062004 <136068548+TrongTan19062004@users.noreply.github.com>
2025-05-03 13:41:46 +0700

    Upload AccountBL.cs

--- LibrarySystemManagement-master/BussinessLayer/AccountBL.cs
+++ LibrarySystemManagement-master/BussinessLayer/AccountBL.cs
@@ -9,6 +9,27 @@ namespace BussinessLayer
 {
     public class AccountBL
     {
+        public AccountBL() { }
+        private AccountDL accountDL=new AccountDL();
+        public List<Account> getAccounts(){
+            return accountDL.GetAccounts();
+        }
+        public bool  addAccount(Account account,String role)
+        {
+            return accountDL.addUser(account,role) > 0;
+        }
+        public bool UpdateAccount(Account account, string role)
+        {
+            return accountDL.UpdateUser(account, role) > 0;
+        }
+        public bool DeleteAccount(int userId)
+        {
+            return accountDL.DeleteUser(userId) > 0;
+        }
+        public Account GetAccountById(int userId)
+        {
+            return accountDL.FindUserById(userId);
+        }
 
     }
 }
